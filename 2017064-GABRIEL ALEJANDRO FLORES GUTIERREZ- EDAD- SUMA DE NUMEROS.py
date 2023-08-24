b21ba55e233397413f3ca186b195b9a95d0d75ed
Gabriel Alejandro Flores Gutierrez <142535427+GabrielThinner@users.noreply.github.com>
2023-08-24 16:49:18 -0600

    Add files via upload

---
+++ 2017064-GABRIEL ALEJANDRO FLORES GUTIERREZ- EDAD- SUMA DE NUMEROS.py
@@ -0,0 +1,7 @@
+#Elabor un programa que sea la suma de dos numeros.
+
+vNum1 = int(input("Escribe el numero 1 "))
+vNum2 = int(input("Escribe el numero 2 "))
+vsuma = vNum1 + vNum2
+print ("La suma de los numeros es = a",vsuma)
+
